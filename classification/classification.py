c073762c22108c7996747ed77d1ac726db1e233e
Karel van Doesburg <karelvandoesburg@gmail.com>
2019-04-01 15:03:35 +0200

    able to read class from truth.jsonl

--- classification/classification.py
+++ classification/classification.py
@@ -8,6 +8,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import precision_score
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import recall_score
+import jsonlines
+import json
 
 
 iris = datasets.load_iris()
@@ -15,20 +17,27 @@ X = iris.data
 y = iris.target
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-print(y_train)
-
 dtrain = xgb.DMatrix(X_train, label=y_train)
 dtest = xgb.DMatrix(X_test, label=y_test)
 
-# dtrain = xgb.DMatrix('../../NLP_data/useritemIds_train_itemsplit_fold1of5.csv?format=csv&label_column=0')
-# dtest = xgb.DMatrix('../../NLP_data/useritemIds_test_itemsplit_fold3of5.csv?format=csv&label_column=0')
+# dtrain = xgb.DMatrix('../../NLP_data/dummy/useritemIds_train_itemsplit_fold1of5.csv?format=csv&label_column=0')
+# dtest = xgb.DMatrix('../../NLP_data/dummy/useritemIds_test_itemsplit_fold3of5.csv?format=csv&label_column=0')
+
+truth = []
+with jsonlines.open('../../NLP_data/truth.jsonl') as json_file:
+    for obj in json_file:
+        if obj['truthClass'] == 'no-clickbait':
+            truth.append(0)
+        else:
+            truth.append(1)
 
 param = {
     'max_depth': 3,  # the maximum depth of each tree
     'eta': 0.3,  # the training step for each iteration
     'silent': 1,  # logging mode - quiet
     'objective': 'multi:softprob',  # error evaluation for multiclass training
-    'num_class': 3}  # the number of classes that exist in this datset
+    'num_class': 3 # the number of classes that exist in this datset
+}
 
 bst = xgb.train(param, dtrain)
 # make prediction
