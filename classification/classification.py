d0814fd513f0dff31e4097103ec094dd2f3c68c1
Karel van Doesburg <karelvandoesburg@gmail.com>
2019-04-02 12:26:15 +0200

    parameter sweep works, only max_depth ready yet

--- classification/classification.py
+++ classification/classification.py
@@ -61,28 +61,40 @@ def train_model(dtrain, dtest, labels_test, params):
     accuracy = accuracy_score(labels_test, best_preds)
     recall = recall_score(labels_test, best_preds, average='macro')
     precision = precision_score(labels_test, best_preds, average='macro')
+    auc = roc_auc_score(labels_test, best_preds, average="macro")
 
     # print("Number of posts classified as clickbait: %s" % np.count_nonzero(best_preds))
     # print("precision: %s" % precision)
     # print("recall: %s" % recall)
     # print("accuracy: %s" % accuracy)
+    # print("auc: %s" % auc)
 
-    return [precision, recall, accuracy]
+    return [precision, recall, accuracy, auc]
 
 
 def parameter_sweep(dtrain, dtest, labels_test):
     accuracy = 0;
     recall = 0;
     precision = 0;
+    auc = 0;
 
-    accuracy_params = [0, 0, 0, 0, 0]
-    recall_params = [0, 0, 0, 0, 0]
-    precision_params = [0, 0, 0, 0, 0]
+    accuracy_params = [0, 0, 0, 0]
+    recall_params = [0, 0, 0, 0]
+    precision_params = [0, 0, 0, 0]
+    auc_params = [0, 0, 0, 0]
 
     max_depth_poss = np.arange(1, 10, 1)
+    min_child_weight_poss = np.arange(1, 6, 1)
+    gamma_poss = np.arange(0.1, 1, 0.1)
+    eta_poss = np.arange(0.1, 0.1, 2)
 
     for max_depth in max_depth_poss:
-        print(max_depth)
+        # for min_child_weight in min_child_weight_poss:
+        #     for gamma in gamma_poss:
+        #         for eta in eta_poss:
+
+
+
         params = {
             'max_depth': max_depth,  # the maximum depth of each tree,
             'min_child_weight': 3,
@@ -97,7 +109,7 @@ def parameter_sweep(dtrain, dtest, labels_test):
             'num_class': 2
         }
 
-        [precision_new, recall_new, accuracy_new] = train_model(dtrain, dtest, labels_test, params)
+        [precision_new, recall_new, accuracy_new, auc_new] = train_model(dtrain, dtest, labels_test, params)
 
         if precision_new > precision:
             precision = precision_new
@@ -111,32 +123,42 @@ def parameter_sweep(dtrain, dtest, labels_test):
             recall = recall_new
             recall_params = [max_depth, 0, 0, 0, 0]
 
-    return[accuracy, precision, recall, accuracy_params, recall_params, precision_params]
+        if auc_new > auc:
+            auc = auc_new
+            auc_params = [max_depth, 0, 0, 0, 0]
+
+    return[accuracy, precision, recall, auc, accuracy_params, recall_params, precision_params, auc_params]
 
 
 # params = {
 #     'max_depth': 5,  # the maximum depth of each tree,
 #     'min_child_weight': 3,
-#     'gamma': 2,
+#     'gamma': 1,
 #     'subsample': 0.8,
 #     'colsample_bytree': 0.8,
 #     'scale_pos_weight': 1,
-#     'eta': 1,  # the training step for each iteration
+#     'eta': 1.6,  # the training step for each iteration
 #     'silent': 1,  # logging mode - quiet
 #     # 'objective': 'binary:logistic'
 #     'objective': 'multi:softprob',
 #     'num_class': 2
 # }
 #
-# [accuracy, precision, recall] = train_model(params)
+# data = get_data()
+# labels = get_labels()
+#
+# [dtrain, dtest, labels_test] = create_DMatrices(data, labels, 0.2)
+#
+# [accuracy, precision, recall] = train_model(dtrain, dtest, labels_test, params)
 
 data = get_data()
 labels = get_labels()
 
 [dtrain, dtest, labels_test] = create_DMatrices(data, labels, 0.2)
 
-[accuracy, precision, recall, accuracy_params, recall_params, precision_params] = parameter_sweep(dtrain, dtest, labels_test)
+[accuracy, precision, recall, auc, accuracy_params, recall_params, precision_params, auc_params] = parameter_sweep(dtrain, dtest, labels_test)
 
+print("")
 print("best accuracy: %s" % accuracy)
 print("with params: %s" % accuracy_params)
 print("")
@@ -145,4 +167,21 @@ print("with params: %s" %  precision_params)
 print("")
 print("best recall: %s" % recall)
 print("with params: %s" % recall_params)
+print("")
+print("best auc: %s" % auc)
+print("with params: %s" % auc_params)
+
+output_file = open('../parameter_sweep_results.txt', 'w')
+
+output_file.write("params: [max_depth, min_child_weight, gamma, eta] \n \n")
+output_file.write("best accuracy: %s \n" % accuracy)
+output_file.write("with params: %s \n \n" % accuracy_params)
+output_file.write("best precision: %s \n" % precision)
+output_file.write("with params: %s \n \n" %  precision_params)
+output_file.write("best recall: %s \n" % recall)
+output_file.write("with params: %s \n \n" % recall_params)
+output_file.write("best auc: %s \n" % auc)
+output_file.write("with params: %s \n \n" % auc_params)
+
+output_file.close()
 
