2a1531db23ab76a6ad571e0b375b2b4e932d99a2
Karel van Doesburg <karelvandoesburg@gmail.com>
2019-04-02 11:50:32 +0200

    parameter sweep works, but still very slow

--- classification/classification.py
+++ classification/classification.py
@@ -62,26 +62,86 @@ def train_model(params):
     preds = bst.predict(dtest)
     best_preds = np.asarray([np.argmax(line) for line in preds])
 
-    print(np.count_nonzero(best_preds))
-    print(precision_score(labels_test, best_preds, average='macro'))
-    print(recall_score(labels_test, best_preds, average='macro'))
-    print(accuracy_score(labels_test, best_preds))
+    accuracy = accuracy_score(labels_test, best_preds)
+    recall = recall_score(labels_test, best_preds, average='macro')
+    precision = precision_score(labels_test, best_preds, average='macro')
 
-    return best_preds
+    print("Number of posts classified as clickbait: %s" % np.count_nonzero(best_preds))
+    print("precision: %s" % precision)
+    print("recall: %s" % recall)
+    print("accuracy: %s" % accuracy)
+
+    return [precision, recall, accuracy]
 
 
-params = {
-    'max_depth': 5,  # the maximum depth of each tree,
-    'min_child_weight': 3,
-    'gamma': 2,
-    'subsample': 0.8,
-    'colsample_bytree': 0.8,
-    'scale_pos_weight': 1,
-    'eta': 1,  # the training step for each iteration
-    'silent': 1,  # logging mode - quiet
-    # 'objective': 'binary:logistic'
-    'objective': 'multi:softprob',
-    'num_class': 2
-}
+def parameter_sweep():
+    accuracy = 0;
+    recall = 0;
+    precision = 0;
 
-best_preds = train_model(params)
+    accuracy_params = [0, 0, 0, 0, 0]
+    recall_params = [0, 0, 0, 0, 0]
+    precision_params = [0, 0, 0, 0, 0]
+
+    max_depth_poss = np.arange(1, 10, 1)
+
+    for max_depth in max_depth_poss:
+        print(max_depth)
+        params = {
+            'max_depth': max_depth,  # the maximum depth of each tree,
+            'min_child_weight': 3,
+            'gamma': 2,
+            'subsample': 0.8,
+            'colsample_bytree': 0.8,
+            'scale_pos_weight': 1,
+            'eta': 1,  # the training step for each iteration
+            'silent': 1,  # logging mode - quiet
+            # 'objective': 'binary:logistic'
+            'objective': 'multi:softprob',
+            'num_class': 2
+        }
+
+        [precision_new, recall_new, accuracy_new] = train_model(params)
+
+        if precision_new > precision:
+            precision = precision_new
+            precision_params = [max_depth, 0, 0, 0, 0]
+
+        if accuracy_new > accuracy:
+            accuracy = accuracy_new
+            accuracy_params = [max_depth, 0, 0, 0, 0]
+
+        if recall_new > recall:
+            recall = recall_new
+            recall_params = [max_depth, 0, 0, 0, 0]
+
+    return[accuracy, precision, recall, accuracy_params, recall_params, precision_params]
+
+
+# params = {
+#     'max_depth': 5,  # the maximum depth of each tree,
+#     'min_child_weight': 3,
+#     'gamma': 2,
+#     'subsample': 0.8,
+#     'colsample_bytree': 0.8,
+#     'scale_pos_weight': 1,
+#     'eta': 1,  # the training step for each iteration
+#     'silent': 1,  # logging mode - quiet
+#     # 'objective': 'binary:logistic'
+#     'objective': 'multi:softprob',
+#     'num_class': 2
+# }
+#
+# [accuracy, precision, recall] = train_model(params)
+
+[accuracy, precision, recall, accuracy_params, recall_params, precision_params] = parameter_sweep()
+
+# print("best accuracy: %s" % accuracy)
+# print("with params: %s" % accuracy_params)
+# print("")
+# print("best precision: %s" % precision)
+# print("with params: %s" %  precision_params)
+# print("")
+# print("best recall: %s" % recall)
+# print("with params: %s" % recall_params)
+
