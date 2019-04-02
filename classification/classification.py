025973c2b4c2fd6c24172bd5d462a47cc34683df
Karel van Doesburg <karelvandoesburg@gmail.com>
2019-04-02 10:16:56 +0200

    converts csv string values to floats, still does not work though

--- classification/classification.py
+++ classification/classification.py
@@ -12,6 +12,21 @@ from sklearn.metrics import roc_auc_score
 import jsonlines
 import csv
 
+
+# iris = datasets.load_iris()
+# X = iris.data
+# y = iris.target
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+#
+# dtrain = xgb.DMatrix(X_train, label=y_train)
+# dtest = xgb.DMatrix(X_test, label=y_test)
+#
+#
+# print(y)
+
+
+
+
 with open('../../NLP_data/preprocessed.csv') as file:
     datafile = csv.reader(file, delimiter=";")
     data = []
@@ -20,17 +35,22 @@ with open('../../NLP_data/preprocessed.csv') as file:
         i = i + 1
         if row and i > 1:  # I have the feeling it skips 2i every time here but I don't know why
             row_no_id = row[1:]
-            data.append(row_no_id)
+            row_int = []
+            for value in row_no_id:
+                row_int.append(float(value))
+            data.append(row_int)
 
 data = np.array(data)
+
+print(data[0])
 
 truth = []
 with jsonlines.open('../../NLP_data/truth.jsonl') as json_file:
     for obj in json_file:
         if obj['truthClass'] == 'clickbait':
-            truth.append(1)
-        else:
             truth.append(0)
+        else:
+            truth.append(1)
 
 truth = np.array(truth)
 print(truth)
@@ -56,7 +76,9 @@ param = {
     'scale_pos_weight': 1,
     'eta': 0.3,  # the training step for each iteration
     'silent': 1,  # logging mode - quiet
-    'objective': 'binary:logistic',  # error evaluation for two-class training
+    'objective': 'binary:logistic'
+    # 'objective': 'multi:softprob',
+    # 'num_class': 3
 }
 
 bst = xgb.train(param, dtrain)
@@ -64,25 +86,14 @@ bst = xgb.train(param, dtrain)
 preds = bst.predict(dtest)
 best_preds = np.asarray([np.argmax(line) for line in preds])
 
-for number in best_preds:
-    print(number)
+# print(best_preds)
+# print(precision_score(y_test, best_preds, average='macro'))
+# print(recall_score(y_test, best_preds, average='macro'))
+# print(accuracy_score(y_test, best_preds))
+
 
 print(best_preds)
 print(precision_score(labels_test, best_preds, average='macro'))
 print(recall_score(labels_test, best_preds, average='macro'))
 print(accuracy_score(labels_test, best_preds))
 print(roc_auc_score(labels_test, best_preds))
-
-
-
-
-# iris = datasets.load_iris()
-# X = iris.data
-# y = iris.target
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-#
-# dtrain = xgb.DMatrix(X_train, label=y_train)
-# dtest = xgb.DMatrix(X_test, label=y_test)
-#
-# dtrain = xgb.DMatrix('../../NLP_data/dummy/useritemIds_train_itemsplit_fold1of5.csv?format=csv&label_column=0')
-# dtest = xgb.DMatrix('../../NLP_data/dummy/useritemIds_test_itemsplit_fold3of5.csv?format=csv&label_column=0')
