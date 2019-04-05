8e526b2eb6d1d9324f9d58bdddaa1fd463cd90f8
Karel van Doesburg <karelvandoesburg@gmail.com>
2019-04-05 14:00:05 +0200

    gainz

--- classification/gain.py
+++ classification/gain.py
@@ -16,12 +16,22 @@ def get_column(data, column_index):
 
 def calc_gains(data):
     gains = []
-    for column_index in range(0, 1):
+    for column_index in range(0, 3):
         print(column_index)
         column = np.array(get_column(data, column_index))
         gain = info_gain.info_gain(labels, column)
-        gains.append(gain)
+        gains.append([column_index, gain])
     return gains
 
 
-print(calc_gains(data))
+def output_gains(gains):
+    output_file = open('../gains_results.txt', 'w')
+
+    for index in range(0, len(gains)):
+        output_file.write("feature %d, gain %.5f" % (gains[index][0], gains[index][1]))
+    output_file.close()
+
+
+gains = calc_gains(data)
+print(gains)
+output_gains(gains)
